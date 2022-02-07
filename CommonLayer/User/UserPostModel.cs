920294ba169e6cb689ecb08f85a849259d582751
Roopa Jamadar <roopajamadar14@gmail.com>
2022-02-08 00:26:57 +0530

    [Roopa] Added: Regex Pattern is Done

--- CommonLayer/User/UserPostModel.cs
+++ CommonLayer/User/UserPostModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CommonLayer.User
@@ -10,9 +11,16 @@ namespace CommonLayer.User
         public string Lastname { get; set; }
         public string PhoneNumber { get; set; }
         public string address { get; set; }
+        [RegularExpression(@"^[a-z0-9]+(.[a-z0-9]+)?@[a-z]+[.][a-z]{3}$",
+         ErrorMessage = "Please enter correct email address")]
         public string Email { get; set; }
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+         ErrorMessage = "Please enter Strong Password")]
         public string password { get; set; }
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+         ErrorMessage = "Please enter Strong Password")]
         public string cpassword { get; set; }
-
+      
     }
+
 }
